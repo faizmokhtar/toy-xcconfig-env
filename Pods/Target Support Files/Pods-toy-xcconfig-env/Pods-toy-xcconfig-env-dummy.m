d85df44218bd3f77d769cef1d2e98b5ee7174b53
Faiz Mokhtar <mfmokhtar@gmail.com>
2018-08-06 15:54:40 +0800

    Setup pods and schemes

---
+++ Pods/Target Support Files/Pods-toy-xcconfig-env/Pods-toy-xcconfig-env-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_toy_xcconfig_env : NSObject
+@end
+@implementation PodsDummy_Pods_toy_xcconfig_env
+@end
---
+++ Pods/Target Support Files/Pods-toy-xcconfig-envTests/Pods-toy-xcconfig-envTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_toy_xcconfig_envTests : NSObject
+@end
+@implementation PodsDummy_Pods_toy_xcconfig_envTests
+@end
